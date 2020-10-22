445d1eeb016998e413943839123103f660f2c489
Beluiart <belyaew.art@yandex.ru>
2020-10-22 22:05:14 +0400

    Added: reviews slider

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-var hotelSlider = new Swiper('.hotel-slider', {
+var hotelSlider = new Swiper(".hotel-slider", {
     // Optional parameters
     loop: true,
     // Navigation arrows
     navigation: {
-      nextEl: '.hotel-slider__button--next',
-      prevEl: '.hotel-slider__button--prev',
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
     },
   });
-  var reviewsSlider = new Swiper('.reviews-slider', {
+  var reviewsSlider = new Swiper(".reviews-slider", {
     // Optional parameters
     loop: true,
     // Navigation arrows
     navigation: {
-      nextEl: '.reviews-slider__button--next',
-      prevEl: '.reviews-slider__button--prev',
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
     },
   });
