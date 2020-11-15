670ae515b3462625f319bfdba0de8cc9506736ee
Beluiart <belyaew.art@yandex.ru>
2020-11-15 22:18:34 +0400

    Create: mobile menu

--- js/main.js
+++ js/main.js
@@ -16,3 +16,10 @@ var hotelSlider = new Swiper(".hotel-slider", {
       prevEl: ".reviews-slider__button--prev",
     },
   });
+
+
+  var menuButton = document.querySelector(".menu-button")
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  });
