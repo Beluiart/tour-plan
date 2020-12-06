25294407fa0dc2c2651f29d9d4380e9b6067cc5b
Beluiart <belyaew.art@yandex.ru>
2020-12-06 18:50:50 +0400

    Fix: modal & footer

--- js/main.js
+++ js/main.js
@@ -19,20 +19,45 @@ $(document).ready(function (){
   });
 
 
-  var menuButton = document.querySelector(".menu-button")
-  menuButton.addEventListener("click", function () {
-    console.log("Клик по кнопке меню");
-    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  var menuButton = $(".menu-button")
+  menuButton.on("click", function () {
+  $(".navbar-bottom").toggleClass("navbar-bottom--visible");
   });
 
-  var modalButton = $("[data-toggle=modal]")
-  console.log(modalButton)
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal)
 
   function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");    
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");    
+  }
+  function closeModal(event) {
+    event.preventDefault(); 
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
-    modalOverlay.addClass(".modal__overlay--visible");    
-    modalDialog.addClass(".modal__dialog--visible");    
+    modalOverlay.removeClass("modal__overlay--visible");    
+    modalDialog.removeClass("modal__dialog--visible");    
   }
+
+  $(".form").each(function(){
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "Name must be at least two letters",
+      },
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com"  
+      },
+      phone: 
+      { required: "Please specify your phone"
+     },  
+    },
+  });
+  });
 });
